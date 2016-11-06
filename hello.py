3ff7f5b6ce9f371aac023cbed3c37c65a4e2b790
Ferris <fynet86@gmail.com>
2016-11-06 23:35:29 +0800

    增加模板渲染框架

--- hello.py
+++ hello.py
@@ -1,29 +1,38 @@
 #!/usr/local/python
 # coding = utf-8
 
-from flask import Flask
+from flask import Flask, render_template
 from flask import request
 from flask import make_response
 from flask import redirect
-from flask.ext.script import Manager
+from flask_bootstrap import Bootstrap
+
+
+
 
 app = Flask(__name__)
-manager = Manager(app)
+
+bootstrap = Bootstrap(app)
 
 @app.route('/')
 def index():
-    response = make_response('<h1>This document carries a cookie!</h1>')
-    response.set_cookie('answer','42')
-    return response
+    return render_template('index.html')
+
+
 @app.route('/user/<name>')
 def user(name):
-    return '<h1>Hello,%s!' % name
+    return render_template('user.html',name=name)
 @app.route('/baidu')
 def baidu():
     return redirect('https://www.baidu.com')
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
 
-
+@app.errorhandler(500)
+def page_server_error(e):
+    return render_template('500.html'), 500
 
 if __name__ == '__main__':
     app.run(host=app.config.get('HOST',None),port=app.config.get('PORT',None))
