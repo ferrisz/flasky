e375f7cdedfe36b77ee56634cf9d86bb5211002e
Ferris <fynet86@gmail.com>
2016-11-14 01:00:36 +0800

    增加修改邮箱功能

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -47,7 +47,7 @@ class PasswordResetRequestForm(Form):
     submit = SubmitField('Reset Password')
 
 class PasswordResetForm(Form):
-    email = StringField('Email', validators=[DataRequired(),Length(1, 64),
+    email = StringField('Email', validators=[DataRequired(), Length(1, 64),
                                              Email()])
     password = PasswordField('Password', validators=[
         DataRequired(), EqualTo('password2', message='Passwords must match')])
@@ -57,3 +57,13 @@ class PasswordResetForm(Form):
     def validate_email(self, field):
         if User.query.filter_by(email=field.data).first() is None:
             raise ValidationError('Unknown email address.')
+
+class ChangeEmailForm(Form):
+    email = StringField('New Email', validators=[DataRequired(), Length(1,64),
+                                             Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Update Email Address')
+
+    def validate_email(self, field):
+        if User.query.filter_by(email=field.data).first():
+            raise ValidationError('Email already registered.')
