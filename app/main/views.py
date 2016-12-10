710042e15c78c6e1ab14c357626d9ba1bc45638f
Ferris <fynet86@gmail.com>
2016-12-10 15:42:34 +0800

    提交和显示评论

--- app/main/views.py
+++ app/main/views.py
@@ -11,9 +11,9 @@ from flask_login import login_required, current_user
 
 from app.decorators import admin_required, permission_required
 from .. import db
-from ..models import User, Role, Permission, Post
+from ..models import User, Role, Permission, Post, Comment
 from . import main
-from .forms import  EditProfileForm, EditProfileAdminForm, PostForm
+from .forms import  EditProfileForm, EditProfileAdminForm, PostForm, CommentForm
 
 
 @main.route('/', methods=['GET', 'POST'])
@@ -94,10 +94,28 @@ def edit_profile_admin(id):
 
     return render_template('edit_profile.html',form=form,user=user)
 
-@main.route('/post/<int:id>')
+@main.route('/post/<int:id>', methods=['GET', 'POST'])
 def post(id):
     post = Post.query.get_or_404(id)
-    return render_template('post.html', posts=[post])
+    form = CommentForm()
+    if form.validate_on_submit():
+        comment = Comment(body=form.body.data,
+                          post=post,
+                          author=current_user._get_current_object())
+        db.session.add(comment)
+        flash(u'评论成功')
+        return redirect(url_for('.post', id=post.id, page=-1))
+    page = request.args.get('page', 1, type=int)
+    if page == -1:
+        page = (post.comments.count() - 1) / \
+            current_app.config['FLASKY_COMMENTS_PER_PAGE'] + 1
+    pagination = post.comments.order_by(Comment.timestamp.asc()).paginate(
+        page, per_page=current_app.config['FLASKY_COMMENTS_PER_PAGE'],
+        error_out=False
+    )
+    comments = pagination.items
+    return render_template('post.html', posts=[post], form=form,
+                           comments=comments, pagination=pagination)
 
 @main.route('/edit/<int:id>', methods=['GET','POST'])
 @login_required
--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@
 
 import os
 from app import create_app, db
-from app.models import User, Role, Follow, Permission, Post
+from app.models import User, Role, Follow, Permission, Post, Comment
 from flask_script import Manager, Shell
 from flask_migrate import Migrate, MigrateCommand
 
@@ -15,7 +15,7 @@ migrate = Migrate(app, db)
 
 def make_shell_context():
     return dict(app=app, db=db, User=User, Follow=Follow,
-                Permission=Permission, Post=Post, Role=Role)
+                Permission=Permission, Post=Post, Role=Role, Comment=Comment)
 manager.add_command("shell",Shell(make_context=make_shell_context))
 manager.add_command("db",MigrateCommand)
 
